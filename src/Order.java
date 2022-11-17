a4dbabffb86f391d628156fdd021acb5d0579232
sselfles <95219729+sselfles@users.noreply.github.com>
2022-11-18 00:00:49 +0800

    fixing

--- src/Order.java
+++ src/Order.java
@@ -58,15 +58,6 @@ public class Order {
         this.flavors = flavors;
     }
     
-    public Order(String order_id, String quantity, String product_name, String total_price, Boolean has_addOns, String flavors){
-        this.order_id = order_id;
-        this.quantity = quantity;
-        this.product_name = product_name;
-        this.total_price = total_price;
-        this.has_addOns = has_addOns;
-        this.flavors = flavors;
-    }
-    
        
     
     public String getProduct_id() {
